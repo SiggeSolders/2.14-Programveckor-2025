71e9c92d97407785876e5abfbe000647199246d8
SiggeSolders <150425641+SiggeSolders@users.noreply.github.com>
2025-01-22 21:01:55 +0100

    Fix to minimap and Pipe

--- Assets/PipelSound.cs
+++ Assets/PipelSound.cs
@@ -1,8 +1,21 @@
+using System.Collections;
 using UnityEngine;
 
 public class PipelSound : MonoBehaviour
 {
     [SerializeField] AudioSource Pipe;
+
+    private void Start()
+    {
+        StartCoroutine(wait());
+    }
+
+    IEnumerator wait()
+    {
+        Pipe.volume = 0;
+        yield return new WaitForSeconds(3);
+        Pipe.volume = 0.5f;
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (Pipe.isPlaying)
