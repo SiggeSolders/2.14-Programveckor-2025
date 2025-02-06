00a68fcf64178831d4abcf952b5d79cb8f48a5e3
SiggeSolders <sigge.hallquistsolders@elev.ga.lbs.se>
2025-02-06 12:16:26 +0100

    lååååång dag
    yeesh

---
+++ Assets/Sova.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Sova : MonoBehaviour
+{
+    public day_night day_Night_;
+
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
